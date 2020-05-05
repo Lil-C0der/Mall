51e3e84064ba0f787c4d2dcc99a6adbb4257181e
Lil-C0der <781172643@qq.com>
2020-05-05 11:32:10 +0800

    fix:改正了一些拼写错误

--- src/components/common/toast/index.js
+++ src/components/common/toast/index.js
@@ -3,9 +3,9 @@ import Toast from "./Toast.vue";
 const obj = {};
 obj.install = function(Vue) {
   // 创建组件的构造器
-  const toastConstrutor = Vue.extend(Toast);
+  const toastConstructor = Vue.extend(Toast);
   // 创建一个toast实例对象
-  const toast = new toastConstrutor();
+  const toast = new toastConstructor();
   // 将toast实例挂载到一个元素上
   toast.$mount(document.createElement("div"));
   document.body.appendChild(toast.$el);
